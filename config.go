7db1d5ee85ff7a823dee564eb44d03ed37ad2b4f
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-04-18 17:26:39 -0400

    update examples
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- config.go
+++ config.go
@@ -6,8 +6,29 @@ import (
 
 func setDefaults() {
 	viper.SetDefault("foods", map[string]string{
-		"fried chicken": string(MAIN_COURSE),
-		"mango pudding": string(DESSERT),
+		// 7 Appetizers
+		"nigiri sushi":   string(APPETIZER),
+		"tofu":           string(APPETIZER),
+		"salad":          string(APPETIZER),
+		"kimchi":         string(APPETIZER),
+		"guacamole":      string(APPETIZER),
+		"tortilla chips": string(APPETIZER),
+		"sausages":       string(APPETIZER),
+		// 6 Main Course Meals
+		"steak":           string(MAIN_COURSE),
+		"sushimi platter": string(MAIN_COURSE),
+		"mac n' cheese":   string(MAIN_COURSE),
+		"fried chicken":   string(MAIN_COURSE),
+		"roasted duck":    string(MAIN_COURSE),
+		"pork belly":      string(MAIN_COURSE),
+		// 7 Desserts
+		"mango pudding":      string(DESSERT),
+		"apple pie":          string(DESSERT),
+		"fruit":              string(DESSERT),
+		"chocolate cake":     string(DESSERT),
+		"tiramisu":           string(DESSERT),
+		"red bean ice cream": string(DESSERT),
+		"strawberry cupcake": string(DESSERT),
 	})
 }
 
@@ -22,7 +43,7 @@ func setupConfig() {
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			// Config file not found; ignore error if desired
-			viper.SafeWriteConfig()
+			// viper.SafeWriteConfig() // could create a new config with the defaults
 		} else {
 			// Config file was found but another error was produced
 			panic(err)
